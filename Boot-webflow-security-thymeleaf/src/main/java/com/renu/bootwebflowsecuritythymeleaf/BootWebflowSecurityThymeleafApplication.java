a6d65768ebcbc35b4411da3350fcb4d68b39c9be
Atif Aslam <aslam.renu.soft43@gmail.com>
2018-07-25 02:03:15 -0800

    Update has been completed successfully

---
+++ Boot-webflow-security-thymeleaf/src/main/java/com/renu/bootwebflowsecuritythymeleaf/BootWebflowSecurityThymeleafApplication.java
@@ -0,0 +1,12 @@
+package com.renu.bootwebflowsecuritythymeleaf;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BootWebflowSecurityThymeleafApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BootWebflowSecurityThymeleafApplication.class, args);
+	}
+}
