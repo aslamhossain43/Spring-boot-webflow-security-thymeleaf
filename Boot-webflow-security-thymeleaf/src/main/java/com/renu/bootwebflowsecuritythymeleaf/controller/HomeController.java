efab67007c91b95f436d4a35290311329ad8cd83
Atif Aslam <aslam.renu.soft43@gmail.com>
2018-07-25 16:01:38 -0800

    Update has been completed successfully

---
+++ Boot-webflow-security-thymeleaf/src/main/java/com/renu/bootwebflowsecuritythymeleaf/controller/HomeController.java
@@ -0,0 +1,18 @@
+package com.renu.bootwebflowsecuritythymeleaf.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+
+@Controller
+public class HomeController {
+
+	@RequestMapping(method = RequestMethod.GET, value = "/")
+	public String index(Model model) {
+		model.addAttribute("message", "Hello, world!");
+		return "home";
+	}
+	
+	
+}
