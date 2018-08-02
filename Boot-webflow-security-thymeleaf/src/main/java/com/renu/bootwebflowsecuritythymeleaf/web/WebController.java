df62775f67998736578e2ad4c0f330bf2fe06d6e
Atif Aslam <aslam.renu.soft43@gmail.com>
2018-08-01 17:42:12 -0800

    Update has been completed successfully

--- Boot-webflow-security-thymeleaf/src/main/java/com/renu/bootwebflowsecuritythymeleaf/web/WebController.java
+++ Boot-webflow-security-thymeleaf/src/main/java/com/renu/bootwebflowsecuritythymeleaf/web/WebController.java
@@ -14,62 +14,61 @@ import com.renu.bootwebflowsecuritythymeleaf.web.model.User;
 import com.renu.bootwebflowsecuritythymeleaf.web.service.SecurityService;
 import com.renu.bootwebflowsecuritythymeleaf.web.service.UserService;
 
-
 @Controller
 public class WebController {
-  @Autowired
-  UserService userService;
-  
-  @Autowired
-   SecurityService securityService;
-  
-  @RequestMapping(value="/main")
-  public String main(){
-    return "sec-main";
-  }
-  
-  @RequestMapping("/login")
-  public String login(Model model, String error, String logout, HttpServletRequest request ){
-    if (logout != null){
-      model.addAttribute("logout", "You have been logged out successfully.");
-    }
-    return "sec-login";
-  }
-  
-  @RequestMapping(value="/loginError")
-  public String loginError(Model model, String username ){
-    model.addAttribute("error", "Your username and password is invalid.");
-    model.addAttribute("username",username);
-    return "sec-login";
-  }
-  
-  @RequestMapping(value="/registration",method=RequestMethod.GET)
-  public String registration(Model model){
-    model.addAttribute("userForm", new User());
-    return "sec-registration";
-  }
-  
-  @RequestMapping(value="/registration",method=RequestMethod.POST)
-  public String registration(@ModelAttribute("userForm") User userForm, BindingResult bindingResult, 
-		  Model model ,String[] roles ){
-    String password = userForm.getPassword();
-    userService.saveUser(userForm,roles);
-    securityService.autologin(userForm.getUsername(),password);
-    return "redirect:/main";
-  }
-  
-  @RequestMapping("/admin")
-  public String admin(){
-    return "sec-admin";
-  }
-  
-  @RequestMapping("/user")
-  public String user(){
-    return "sec-user";
-  }
-  
-  @RequestMapping("/403")
-  public String access(){
-    return "sec-access";
-  }
+	@Autowired
+	UserService userService;
+
+	@Autowired
+	SecurityService securityService;
+
+	@RequestMapping(value = "/main")
+	public String main() {
+		return "sec-main";
+	}
+
+	@RequestMapping("/login")
+	public String login(Model model, String error, String logout, HttpServletRequest request) {
+		if (logout != null) {
+			model.addAttribute("logout", "You have been logged out successfully.");
+		}
+		return "sec-login";
+	}
+
+	@RequestMapping(value = "/loginError")
+	public String loginError(Model model, String username) {
+		model.addAttribute("error", "Your username and password is invalid.");
+		model.addAttribute("username", username);
+		return "sec-login";
+	}
+
+	@RequestMapping(value = "/registration", method = RequestMethod.GET)
+	public String registration(Model model) {
+		model.addAttribute("userForm", new User());
+		return "sec-registration";
+	}
+
+	@RequestMapping(value = "/registration", method = RequestMethod.POST)
+	public String registration(@ModelAttribute("userForm") User userForm, BindingResult bindingResult, Model model,
+			String[] roles) {
+		String password = userForm.getPassword();
+		userService.saveUser(userForm, roles);
+		securityService.autologin(userForm.getUsername(), password);
+		return "redirect:/main";
+	}
+
+	@RequestMapping("/admin")
+	public String admin() {
+		return "sec-admin";
+	}
+
+	@RequestMapping("/user")
+	public String user() {
+		return "sec-user";
+	}
+
+	@RequestMapping("/403")
+	public String access() {
+		return "sec-access";
+	}
 }
